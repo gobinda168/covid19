7f48c2cf5503cebb94007a8671e5006178bb7752
Gobinda Das <gobinda168@gmail.com>
2020-03-20 13:53:14 +0530

    Updates

---
+++ precache-manifest.5b2c2a3b1863b113c3d8f76ca8a4f86e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aaa84a3f44fdd09dc877ae7c50eff40",
+    "url": "/gobinda168/covid19/index.html"
+  },
+  {
+    "revision": "f577fb4cf425bdb2c833",
+    "url": "/gobinda168/covid19/static/css/main.b10ab0d2.chunk.css"
+  },
+  {
+    "revision": "eff31aa5b67f48990436",
+    "url": "/gobinda168/covid19/static/js/2.c02b4e52.chunk.js"
+  },
+  {
+    "revision": "0f2be52aac348a57b5e1722f475cbfd0",
+    "url": "/gobinda168/covid19/static/js/2.c02b4e52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f577fb4cf425bdb2c833",
+    "url": "/gobinda168/covid19/static/js/main.7d6543d6.chunk.js"
+  },
+  {
+    "revision": "cabf6307212d3a9c9e4c",
+    "url": "/gobinda168/covid19/static/js/runtime-main.0a630048.js"
+  }
+]);
